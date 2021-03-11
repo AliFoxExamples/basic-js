a26d62d7099e06bd7bf2aff99fede14338baeac3
AliFoxExamples <user-ali-sadykov@yandex.ru>
2021-03-11 23:32:57 +0300

    fix: an attempt to fix

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,10 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  return members.map(el=> {
-
-    return typeof el == 'string'? el.trim(): '';
-
-  }).map(el=>el[0]).sort().join('').toUpperCase();
-
+  return members.filter(el=>typeof el == 'string').map(el=>el.trim()[0].toUpperCase()).sort().join('');
 };
