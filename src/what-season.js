c4cc65e87fb5f818be8884e9f6326c271e0f98b9
AliFoxExamples <user-ali-sadykov@yandex.ru>
2021-03-12 00:16:35 +0300

    init: start another task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+
+  if (!arguments.length) {
+    'Unable to determine the time of year!';
+  }
+
+  let bool = Object.prototype.toString.call(date) === "[object Date]";
+
+  if ( !(bool && date.getDate()) ){
+    throw new Error ();
+  }
+  
+  let seasons = {'winter': [11,0,1], 'spring':[2,3,4], 'summer':[5,6,7], 'autumn':[8,9,10]};
+
+  let month = date.getMonth();
+
+  for (let el in seasons) {
+
+    if ( seasons[el].some(it=>it==month) ) {
+      return el;
+    }
+  }
+  
 };
