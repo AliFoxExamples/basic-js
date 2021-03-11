131917b98de284d6bf5836d6e18646c5184861df
AliFoxExamples <user-ali-sadykov@yandex.ru>
2021-03-11 21:59:13 +0300

    fix: an attempt to fix

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,6 +3,7 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(sampleActivity) {
-  return typeof sampleActivity == 'string' && Number.isFinite(+sampleActivity)? Math.ceil(Math.log(15/sampleActivity)/(Math.log(2)/5730)): false;
+module.exports = function dateSample(/*sampleActivity*/) {
+  throw new CustomError('Not implemented');
+  // return typeof sampleActivity == 'string' && Number.isFinite(+sampleActivity)? Math.ceil(Math.log(15/sampleActivity)/(Math.log(2)/5730)): false;
 };
