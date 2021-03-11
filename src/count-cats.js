2b4b13cfad53aac5dfb0e3a47d0f6612ce337803
AliFoxExamples <user-ali-sadykov@yandex.ru>
2021-03-11 22:14:37 +0300

    fix: an attempt to fix

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  return matrix.toString().split("^^").length-1
+  return matrix.toString().split(",").filter(el=>el=="^^").length;
 };
