364d539fa0e9d12483ec1b120d17c379afba063a
AliFoxExamples <user-ali-sadykov@yandex.ru>
2021-03-11 22:51:21 +0300

    fix: an attempt to fix

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity == 'string' &&  Number.isFinite(+sampleActivity) &&  +sampleActivity !== 0) {
+  if (typeof sampleActivity == 'string' &&  Number.isFinite(+sampleActivity) &&  +sampleActivity > 0) {
     let result = Math.ceil(Math.log(15/sampleActivity)/(Math.log(2)/5730));
     return result <=0?  false: result;
   }
