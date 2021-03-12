099a9564bc0fe3e1755378e159870a266bcfa8fe
AliFoxExamples <user-ali-sadykov@yandex.ru>
2021-03-13 00:10:14 +0300

    init: start another task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,9 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  let turns = ((disksNumber-1)**3)-1;
+module.exports = function calculateHanoi (disksNumber, turnsSpeed) {
 
-  let seconds =  Math.floor(turns/(turnsSpeed/3600))
+  throw new CustomError('Not implemented');
 
-  return {turns, seconds}
-};
+//   let arr = [[],[],[]];
+
+//   for (let i = disksNumber; i > 0; i--) {
+
+
+//      arr[0].push(i);
+
+//   }
+
+// //     return arr;
+
+  
+//   let turns = 0;
+
+//   for (let i = 0; ;){
+
+//       if ( arr[i][arr[i].length-1] >  arr[i+1][arr[i+1].length-1] ) {
+
+//           i++;
+
+//       }
+
+//       else {
+
+//           arr[i+1].push(arr[i].pop());
+//           turns++;
+
+//       }
+  
+//   }
+
+
+
+
+
+  
+}
+
+
+
+// function calculateHanoi(disksNumber, turnsSpeed) {
+//   let turns = ((disksNumber-1)**3)-1;
+
+//   let seconds =  Math.floor(turns/(turnsSpeed/3600))
+
+//   return {turns, seconds}
+// }
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Object.prototype.toString.call(arr) !== '[object Array]') {
+
+    throw new Error();
+
+  }
+
+  let target = JSON.parse(JSON.stringify(arr));
+
+  let sequenes = {
+    "--discard-next": function (arr,i) {
+      if (arr[i+1] && (typeof arr[i+1] == 'number')) {
+        return arr.filter((el,k,arr)=>{
+            return !(k==i || k==i+1);
+        })
+      }
+    },
+     "--discard-prev" : function (arr,i){
+      if (arr[i-1] && (typeof arr[i-1] == 'number')) {
+        return arr.filter((el,k,arr)=>{
+            return !(k+1==i || k==i);
+        })
+      }
+     },
+     "--double-next" : function (arr,i){
+      if (arr[i+1] && (typeof arr[i+1] == 'number')) {
+        arr.splice(i,2,arr[i+1]*2);          
+
+        return arr;
+      }
+     },
+     "--double-prev" : function (arr,i){
+      if (arr[i-1] && (typeof arr[i-1] == 'number')) {
+        
+        arr.splice(i-1,2,arr[i-1]*2);          
+
+        return arr;
+      }
+     }
+  }
+  
+
+  for (let i = 0; i < target.length; i++) {
+    if( target[i] in sequenes) {
+        target = sequenes[target[i]](target, i);
+        i = 0;
+    }
+  }
+
+  return target;
 };
