7174441a5e537ae533eab3cadaad2f91f6dde853
AliFoxExamples <user-ali-sadykov@yandex.ru>
2021-03-14 02:35:46 +0300

    init: start recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let depth = 1;
+    for(let el of arr) {
+      let temp = 1;
+      if (Array.isArray(el)) {
+        temp += this.calculateDepth(el)
+      }
+      depth = Math.max(depth, temp)
+    }
+    return depth;
   }
 };
