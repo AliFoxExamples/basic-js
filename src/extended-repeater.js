aa9c2a2cc4b8d326db163de335fade6f4b873ccd
AliFoxExamples <user-ali-sadykov@yandex.ru>
2021-03-14 03:44:21 +0300

    init: start extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  options.separator = options.separator || "+";
+  options.addition = options.addition === undefined? "" : options.addition;
+  options.additionSeparator  = options.additionSeparator || "|";
+
+  str = typeof str == 'string'? str: String(str);
+  options.addition = typeof options.addition == 'string'? options.addition : String(options.addition);
+
+  function parser(str='',n=1,sep='') {
+    return new Array(n).fill(str).join(sep)
+  }
+  let add = parser(options.addition, options.additionRepeatTimes, options.additionSeparator);
+  return parser(str+add, options.repeatTimes, options.separator);
 };
-  
+
+
